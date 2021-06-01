cd8bcbdb78e411718323c02f9029c608f0b0f560
Maximiliano Kolus <mkolus@tux-solutions.com>
2021-06-01 14:30:33 -0300

    conversión de direcciones ip, interface mapping

--- mikrotik/README.md
+++ mikrotik/README.md
@@ -4,8 +4,44 @@ Convierte bloques de configuración de archivos .rsc de Mikrotik a su contrapart
 
 ## Línea de comando
 
+### --mikrotik-config `ARCHIVO.RSC`
+
+Archivo con la configuración del Mikrotik, formato tipo `/export`. No es necesario que esté toda la configuración, solo lo relevante para la conversión que se va a hacer. Por ejemplo, si solo se van a convertir los usuarios de ppp, con la sección `/ppp/secret` alcanza.
+
+### --fortigate-config `ARCHIVO.CONF`
+
+Archivo donde se escribirá el script de FortiOS.
+
 ### --ppp-users
 
 Convierte la sección usuarios de PPP a usuarios locales de FortiGate (*config user local*).
 
+#### Ejemplos
 
+- --dhcp-servers LAN1 LAN2
+- --dhcp-servers *
+
+### --map-interfaces `MKT_IF:FGT_IF ...`
+
+Cambiar las referencias de las interfaces de Mikrotik `MKT_IF` a interfaces de FortiGate `FGT_IF`. Es **case insensitive** para la interfaz de Mikrotik, y **sensitive** para las de FortiGate.
+
+#### Ejemplo
+
+- --map-interfaces ether1:internal ether2:wan1 ether3:wan2
+
+### --addresses `{INTERFACE ...|*}`
+
+Genera el bloque de configuración para direcciones IPs de las interfaces (*config system interface*). Solo se migran las direcciones habilitadas.
+Usar "*" para migrar todas las interfaces.
+
+**IMPORTANTE**: en Mikrotik no existe el concepto de dirección primaria y secundaria. El script las convertirá en el orden que las encuentre y solo usará como primaria la primera que encuentre. Posiblemente requiera retoques manuales.
+
+#### Ejemplo
+
+- --addresses *
+- --addresses ether1 ether2 ether4
+
+### --dhcp-servers `{SERVER ...|*}`
+
+Listado de **DHCP servers y leases** a migrar.
+Usar "*" para migrar todos los DHCP servers. Solo se migrarán aquellos que estén habilitados.
--- mikrotik/mkt2fgt.py
+++ mikrotik/mkt2fgt.py
@@ -1,5 +1,6 @@
 import argparse
 import shlex
+import ipaddress
 
 import pprint
 
@@ -12,10 +13,10 @@ from sys import exit
 parser = argparse.ArgumentParser(description="Converts mikrotik configuration sections to fortigate")
 parser.add_argument("--mikrotik-config", help="mikrotik config file", required=True)
 parser.add_argument("--fortigate-config", help="output forgitate config file", required=True)
-parser.add_argument('--ppp-users', help="convert ppp users to local users", default=False, action='store_true')
+parser.add_argument("--map-interfaces", nargs="*", help="[MKT_IF:FGT_IF ... ]. replace mikrotik interfaces with its fortigate counterparts.")
+parser.add_argument("--ppp-users", help="convert ppp users to local users", default=False, action='store_true')
+parser.add_argument("--addresses", nargs="*", help="[INTERFACE ...] generate interface address configuration", default=False, )
 args = parser.parse_args()
-
-print(args)
 
 # open mikrotik config file
 if isfile(args.mikrotik_config):
@@ -42,6 +43,18 @@ except OSError as e:
         exception=e.strerror
     ))
     
+# interface mapping
+ifmap = dict()
+
+for ifmapping in args.map_interfaces:
+    v = ifmapping.split(":")
+    if len(v) != 2:
+        print("*** ERROR:invalid interface map {ifmapping}, exiting.".format(
+            ifmapping=ifmapping
+        ))
+        exit(-1)
+    
+    ifmap[v[0]] = v[1]
 
 # convert config to dict and lists
 config = dict()
@@ -81,7 +94,7 @@ for l in f.readlines():
 if args.ppp_users == True:
     print (">>> creating local users from ppp users")
     if "/ppp/secret" in config and len(config["/ppp/secret"]) > 0:
-        o.write("config user local\n")
+        o.write("\nconfig user local\n")
         for user_cmd in config["/ppp/secret"]:
             (c,p) = user_cmd
             if c == "add":
@@ -92,6 +105,97 @@ if args.ppp_users == True:
         o.write("end\n")
     else:
         print("!!! no users to migrate")
+
+# addresses
+if args.addresses is not None:
+    
+    if "*" in args.addresses:
+        if len(args.addresses) != 1:
+            print("*** ERROR: you shouldn't specify multiple and use \"*\"")
+            exit(-1)
+        else:
+            print(">>> migrating addresses for all interfaces")
+            all_interfaces = True
+    else:
+        all_interfaces = False
+        interfaces = [x.casefold() for x in args.addresses]
+        print(">>> migrating addresses for {ifs}".format(
+            ifs=", ".join(interfaces)
+        ))
+
+    print (">>> configure interface addresses")
+    if "/ip/address" in config and len(config["/ip/address"]):
+        ifaddress = dict()
+        for address in config["/ip/address"]:
+            # disabled?
+            (cmd, opt) = address
+            if "disabled" in opt and opt["disabled"] == "yes":
+                continue
+
+            if all_interfaces is True or opt["interface"].casefold() in interfaces:
+                # validate ip address
+                try:
+                    ip = ipaddress.ip_address(opt["address"].split("/")[0])
+                    network = ipaddress.ip_network(opt["address"], strict=False)
+                except:
+                    print("*** ERROR: invalid IP address {ip} for interface {interface}, skipping.".format(
+                        ip=opt["address"],
+                        interface=opt["interface"]
+                    ))
+                    continue
+                
+                # interface mapping
+                interface = ifmap[opt["interface"]] if opt["interface"] in ifmap else opt["interface"]
+
+                if interface in ifaddress:
+                    ifaddress[interface].append((ip, network))
+                else:
+                    ifaddress[interface] = [(ip, network)]
+                
+            else:
+                print("--- skipped address {address} for interface {interface}".format(
+                    address=opt["address"],
+                    interface=opt["interface"]
+                ))
+
+        if len(ifaddress) > 0:
+            o.write("\nconfig system interface\n")
+            for interface in ifaddress.keys():
+                # primary ip address
+                (ip, network) = ifaddress[interface][0]
+                o.write("    edit \"{interface}\"\n".format(
+                    interface=interface
+                ))
+                o.write("        set ip {ip} {mask}\n".format(
+                    ip=ip,
+                    mask=network.netmask
+                ))
+                o.write("        set allowaccess ping\n")
+                print("--- configured primary ip {ip} for interface {interface}".format(
+                    ip=str(ip) + "/" + str(network.prefixlen),
+                    interface=interface
+                ))
+                
+                # secondary addresses
+                if len(ifaddress[interface]) > 1:
+                    o.write("        set secondary-IP enable\n        config secondaryip\n")
+                    for sec in ifaddress[interface][1:]:
+                        (ip, network) = sec
+                        o.write("            edit 0\n                set ip {ip} {mask}\n".format(
+                            ip=ip,
+                            mask=network.netmask
+                        ))
+                        o.write("                set allowaccess ping\n            next\n")
+
+                    o.write("        end\n")
+
+                o.write("    next\n")
+                
+            o.write("end")
+        else:
+            print("!!! no interface addresses to migrate")
+    else:
+        print("!!! no interface addresses to migrate")
 
 #
 o.close()
